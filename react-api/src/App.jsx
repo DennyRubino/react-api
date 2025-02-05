305c7c1b37f37b6eacad3ff56a009df1f456f0ae
Denny Rubino <rubidenny@gmail.com>
2025-02-05 22:35:08 +0100

    init project

---
+++ react-api/src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import articlesData from "./assets/Data/ArticleData";
+import "./App.css";
+
+function App() {
+  const [articles, setArticles] = useState(articlesData);
+  const [newArticleTitle, setNewArticleTitle] = useState("");
+  const [newArticleAuthor, setNewArticleAuthor] = useState("");
+  const [newArticleContent, setNewArticleContent] = useState("");
+  const [newArticleCategory, setNewArticleCategory] = useState("categoria");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newArticle = {
+      id: Date.now(),
+      title: newArticleTitle,
+      author: newArticleAuthor,
+      content: newArticleContent,
+      category: newArticleCategory,
+    };
+
+    setArticles([...articles, newArticle]);
+    setNewArticleTitle("");
+    setNewArticleAuthor("");
+    setNewArticleContent("");
+    setNewArticleCategory("categoria");
+  };
+
+  return (
+    <div className="App">
+      <h1>Lista Articoli</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <h2>{article.title}</h2>
+            <p>
+              <strong>Autore:</strong> {article.author}
+            </p>
+            <p>
+              <strong>Categoria:</strong> {article.category}
+            </p>
+            <p>{article.content}</p>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Aggiungi un nuovo contenuto</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Titolo dell'contenuto"
+          value={newArticleTitle}
+          onChange={(e) => setNewArticleTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Autore"
+          value={newArticleAuthor}
+          onChange={(e) => setNewArticleAuthor(e.target.value)}
+        />
+        <textarea
+          placeholder="Contenuto"
+          value={newArticleContent}
+          onChange={(e) => setNewArticleContent(e.target.value)}
+        />
+        <select
+          value={newArticleCategory}
+          onChange={(e) => setNewArticleCategory(e.target.value)}
+        >
+          <option value="categoria"> -</option>
+          <option value="Old">Old</option>
+          <option value="New">New</option>
+        </select>
+        <button type="submit">Aggiungi contenuto</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-api/src/assets/Data/ArticleData.jsx
@@ -0,0 +1,32 @@
+const articlesData = [
+  {
+    id: 1,
+    title: "One piece",
+    author: "Eiichirō Oda",
+    content: "Anime per ragazzi",
+    category: "Old",
+  },
+  {
+    id: 2,
+    title: "Dragon ball",
+    author: "Akira Toriyama",
+    content: "Anime sul combattimento",
+    category: "Old",
+  },
+  {
+    id: 3,
+    title: "Pokemon",
+    author: "Satoshi Tajiri",
+    content: "Anime su creature e ragazzi",
+    category: "Old",
+  },
+  {
+    id: 4,
+    title: "Demon Slayer",
+    author: "Koyoharu Gotōge",
+    content: "Anime sulla caccia ai demoni",
+    category: "New",
+  },
+];
+
+export default articlesData;
